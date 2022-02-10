091bd52aef66c434a3d486234d0d501f5f2d5aac
Ariel Villarreal <avillareal@serempre.com>
2022-02-10 15:14:31 -0300

    feat: init

---
+++ src/data/info.ts
@@ -0,0 +1,14 @@
+const info = {
+	name: "Lic. Patricia Viñas Ugarte",
+	image: "/face.jpeg",
+	titles: ["Psicoterapia", "Astrología", "Cursos/Talleres de Cocreacion y Astrología"],
+	country: "Argentina",
+	contact: {
+		phone: "+541144444110",
+		instagram: "https://www.instagram.com/patriciavinasugarte/",
+		email: "graviu23@gmail.com",
+		facebook: "https://www.facebook.com/patricia.vinasugarte"
+	}
+}
+
+export default info
---
+++ src/pages/asdad.tsx
@@ -0,0 +1,58 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+		<div className="bg-custom min-h-screen flex items-center justify-center">
+			<div className='max-w-lg bg-white shadow-lg rounded-lg p-2'>
+				<div className="space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0">
+					<div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
+						<img className="object-cover shadow-lg rounded-lg w-full" src='/face.jpeg' alt="Patricia Viñas" />
+					</div>
+					<div className="sm:col-span-2">
+						<div className="space-y-4">
+							<div className="text-lg leading-6 font-medium">
+								<h1>Lic. Patricia Viñas Ugarte</h1>
+								<h2 className="text-indigo-600 text-sm">Psicoterapia</h2>
+								<h2 className="text-indigo-600 text-sm">Astrología</h2>
+								<h2 className="text-indigo-600 text-sm">Cursos/Talleres de Cocreacion y Astrología</h2>
+							</div>
+							{/* <div className="text-lg">
+								<p className="text-gray-500">{person.bio}</p>
+							</div>
+							<ul role="list" className="flex space-x-5">
+								<li>
+									<a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
+										<span className="sr-only">Twitter</span>
+										<svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+											<path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+										</svg>
+									</a>
+								</li>
+								<li>
+									<a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
+										<span className="sr-only">LinkedIn</span>
+										<svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+											<path
+												fillRule="evenodd"
+												d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+												clipRule="evenodd"
+											/>
+										</svg>
+									</a>
+								</li>
+							</ul> */}
+						</div>
+					</div>
+				</div>
+			</div>
+			<style jsx>{`
+				.bg-custom {
+					background-color: #DFDBE5;
+					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 56 28' width='56' height='28'%3E%3Cpath fill='%239C92AC' fill-opacity='0.4' d='M56 26v2h-7.75c2.3-1.27 4.94-2 7.75-2zm-26 2a2 2 0 1 0-4 0h-4.09A25.98 25.98 0 0 0 0 16v-2c.67 0 1.34.02 2 .07V14a2 2 0 0 0-2-2v-2a4 4 0 0 1 3.98 3.6 28.09 28.09 0 0 1 2.8-3.86A8 8 0 0 0 0 6V4a9.99 9.99 0 0 1 8.17 4.23c.94-.95 1.96-1.83 3.03-2.63A13.98 13.98 0 0 0 0 0h7.75c2 1.1 3.73 2.63 5.1 4.45 1.12-.72 2.3-1.37 3.53-1.93A20.1 20.1 0 0 0 14.28 0h2.7c.45.56.88 1.14 1.29 1.74 1.3-.48 2.63-.87 4-1.15-.11-.2-.23-.4-.36-.59H26v.07a28.4 28.4 0 0 1 4 0V0h4.09l-.37.59c1.38.28 2.72.67 4.01 1.15.4-.6.84-1.18 1.3-1.74h2.69a20.1 20.1 0 0 0-2.1 2.52c1.23.56 2.41 1.2 3.54 1.93A16.08 16.08 0 0 1 48.25 0H56c-4.58 0-8.65 2.2-11.2 5.6 1.07.8 2.09 1.68 3.03 2.63A9.99 9.99 0 0 1 56 4v2a8 8 0 0 0-6.77 3.74c1.03 1.2 1.97 2.5 2.79 3.86A4 4 0 0 1 56 10v2a2 2 0 0 0-2 2.07 28.4 28.4 0 0 1 2-.07v2c-9.2 0-17.3 4.78-21.91 12H30zM7.75 28H0v-2c2.81 0 5.46.73 7.75 2zM56 20v2c-5.6 0-10.65 2.3-14.28 6h-2.7c4.04-4.89 10.15-8 16.98-8zm-39.03 8h-2.69C10.65 24.3 5.6 22 0 22v-2c6.83 0 12.94 3.11 16.97 8zm15.01-.4a28.09 28.09 0 0 1 2.8-3.86 8 8 0 0 0-13.55 0c1.03 1.2 1.97 2.5 2.79 3.86a4 4 0 0 1 7.96 0zm14.29-11.86c1.3-.48 2.63-.87 4-1.15a25.99 25.99 0 0 0-44.55 0c1.38.28 2.72.67 4.01 1.15a21.98 21.98 0 0 1 36.54 0zm-5.43 2.71c1.13-.72 2.3-1.37 3.54-1.93a19.98 19.98 0 0 0-32.76 0c1.23.56 2.41 1.2 3.54 1.93a15.98 15.98 0 0 1 25.68 0zm-4.67 3.78c.94-.95 1.96-1.83 3.03-2.63a13.98 13.98 0 0 0-22.4 0c1.07.8 2.09 1.68 3.03 2.63a9.99 9.99 0 0 1 16.34 0z'%3E%3C/path%3E%3C/svg%3E");
+				}
+			`}</style>
+		</div>
+  )
+}
+
+export default Home
