aadebe6dbe541f6a83d06ce12b0b75e9364ea10d
Santiago Villarreal <santiago79@pulppo.com>
2022-06-30 17:32:05 -0500

    feat: google analytics and upgrade dependencies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
-  return (
+    return (
     <Html lang="es">
-      <Head />
+      <Head>
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-L9WLRNH9TC"></Script>
+          <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+        
+                  gtag('config', 'G-L9WLRNH9TC');
+              `}
+          </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
